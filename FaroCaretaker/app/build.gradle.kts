ccdaac3681b93deb217ae8ce237331e32d449bd4
summayahjoyia <summayah.joyia@gmail.com>
2026-03-08 10:43:20 -0600

    etc

--- FaroCaretaker/app/build.gradle.kts
+++ FaroCaretaker/app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
     implementation(platform("com.google.firebase:firebase-bom:34.10.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-database")
